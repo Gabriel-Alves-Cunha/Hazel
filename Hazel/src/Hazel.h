4ce170f50b1044c69e1305e57d1e4249c8fd4cfb
Gabriel Alves Cunha <gabriel925486@gmail.com>
2019-12-30 17:42:05 -0300

    Setup basic Application and Logging.

---
+++ Hazel/src/Hazel.h
@@ -0,0 +1,11 @@
+#pragma once
+
+// For use by Hazel apllications
+
+//#include <stdio.h>
+#include "Hazel/Application.h"
+#include "Hazel/Log.h"
+
+// --ENTRY POINT------------------
+#include "Hazel/EntryPoint.h"
+// -------------------------------
